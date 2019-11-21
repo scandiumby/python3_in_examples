00c50b282c1de3a0c582ad2384a49a9ef7267d5c
Egor N. Kostyuk <egor@kostyuk.by>
2019-11-21 19:52:36 +0300

    add get_russian_chars_not_included_in_string(), change test_string/string_standart

--- char_counter_for_string/main.py
+++ char_counter_for_string/main.py
@@ -7,8 +7,15 @@ def get_count_each_chars_in_string(input_string: str) -> dict:
 def get_count_unique_chars_in_string(input_string: str) -> int:
     return len(set(input_string))
 
+
+def get_russian_chars_not_included_in_string(input_string: str) -> set:
+    russian_alphabet = {chr(char_code) for char_code in range(0x410, 0x450)}
+    russian_alphabet.update(('Ё', 'ё'))
+    return russian_alphabet.difference(set(input_string))
+
+
 if __name__ == '__main__':
-    test_string = 'cazbaazzadzdcczz'
+    test_string = 'аааБББрСя'
     print(f'{test_string=}')
     print(f'{get_count_each_chars_in_string(test_string)=}')
     print(f'{get_count_unique_chars_in_string(test_string)=}')
--- char_counter_for_string/utest.py
+++ char_counter_for_string/utest.py
@@ -4,17 +4,16 @@ from char_counter_for_string.main import *
 
 
 class CharCounterTest(TestCase):
-    string_standart = 'cazbaazzadzdcczz' # a - 4, b - 1, c - 3, d - 2, z - 6
+    string_standart = 'аааБББрСя'
+    russian_alphabet = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'
 
     def test_get_count_each_chars_in_string(self):
-        dict_standart = {
-            'a': 4,
-            'b': 1,
-            'c': 3,
-            'd': 2,
-            'z': 6,
-        }
+        dict_standart = {'С': 1, 'Б': 3, 'р': 1, 'я': 1, 'а': 3}
         self.assertDictEqual(dict_standart, get_count_each_chars_in_string(self.string_standart))
 
     def test_get_count_unique_chars_in_string(self):
         self.assertEqual(5, get_count_unique_chars_in_string(self.string_standart))
+
+    def test_get_russian_chars_not_included_in_string(self):
+        not_included_russian_chars = set('АбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯ')
+        self.assertSetEqual(not_included_russian_chars, get_russian_chars_not_included_in_string(self.string_standart))
